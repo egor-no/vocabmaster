cd1e00c5e5104ac278640c7360b8e330c9acad61
egor-no <e.nosko93@ya.ru>
2022-11-11 19:33:40 +0600

    Add word and vocabnulary models with inner classes

--- src/app/vocabmaster/model/Vocab.java
+++ src/app/vocabmaster/model/Vocab.java
@@ -1,4 +1,78 @@
 package app.vocabmaster.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Vocab {
+
+    private String name;
+    private String languageFrom;
+    private String languageTo;
+    private String description;
+    private List<Word> words;
+    private final VocabScheduler scheduler;
+
+    public Vocab() {
+        words = new ArrayList<>();
+        scheduler = new VocabScheduler();
+    }
+
+    public void addWord(Word word) {
+        words.add(word);
+    }
+
+    public List<Word> getTodayWords() {
+        return scheduler.getTodayWords();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguageFrom() {
+        return languageFrom;
+    }
+
+    public void setLanguageFrom(String languageFrom) {
+        this.languageFrom = languageFrom;
+    }
+
+    public String getLanguageTo() {
+        return languageTo;
+    }
+
+    public void setLanguageTo(String languageTo) {
+        this.languageTo = languageTo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Word> getWords() {
+        return words;
+    }
+
+    public void setWords(List<Word> words) {
+        this.words = words;
+    }
+
+    private class VocabScheduler {
+
+        private List<Word> getTodayWords() {
+            return words.stream()
+                    .filter(word -> { return word.getScheduledDate().isBefore(LocalDate.now().plusDays(1)); })
+                    .collect(Collectors.toList());
+        }
+    }
 }
--- src/app/vocabmaster/model/Word.java
+++ src/app/vocabmaster/model/Word.java
@@ -1,4 +1,80 @@
 package app.vocabmaster.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Word {
+
+    private String word;
+    private List<String> translations;
+    private final WordScheduler wordScheduler;
+
+    public Word(String word, String translations) {
+        this.word = word;
+        this.translations = new ArrayList<>();
+        addTranslations(translations);
+        wordScheduler = new WordScheduler();
+    }
+
+    public Word(String word, String translations, int skipDays, LocalDate date) {
+        this.word = word;
+        this.translations = new ArrayList<>();
+        addTranslations(translations);
+        wordScheduler = new WordScheduler(skipDays, date);
+    }
+
+    public void addTranslation(String variant) {
+        translations.add(variant);
+    }
+
+    public void addTranslations(String translations) {
+        String[] variants = translations.split(",");
+        for (int i = 0; i < variants.length; i++) {
+            this.translations.add(variants[i]);
+        }
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public List<String> getTranslations() {
+        return translations;
+    }
+
+    public LocalDate getScheduledDate() {
+        return wordScheduler.nextScheduledDate;
+    }
+
+    public void setScheduledDate(LocalDate newDate) {
+        wordScheduler.nextScheduledDate = newDate;
+    }
+
+    public int getSkipDays() {
+        return wordScheduler.daysSkip;
+    }
+
+    public void setSkipDays(int skipDays) {
+        wordScheduler.daysSkip = skipDays;
+    }
+
+    private class WordScheduler {
+        private int daysSkip;
+        private LocalDate nextScheduledDate;
+
+        private WordScheduler() {
+            daysSkip = 1;
+            nextScheduledDate = LocalDate.now().plusDays(1);
+        }
+
+        private WordScheduler(int daysSkip, LocalDate date) {
+            this.nextScheduledDate = date;
+            this.daysSkip = daysSkip;
+        }
+    }
 }
